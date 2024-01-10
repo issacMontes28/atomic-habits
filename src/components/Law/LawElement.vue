41204ccdf1117cbe3ec0bab98256de3e0ae40e07
issacMontes28 <issac@butchershop.co>
2024-01-09 19:50:47 -0600

    added tailwind

--- src/components/Law/LawElement.vue
+++ src/components/Law/LawElement.vue
@@ -1,18 +1,16 @@
 <template>
-    <header>
-        <h2>{{ name }}</h2>
-    </header>
-    <p>The info for a law</p>
+  <header>
+    <p class="mt-2 text-gray-500">{{ name }}</p>
+  </header>
+  <p>The info for a law</p>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-    props: {
-        name: String
-    }
+  props: {
+    name: String
+  }
 })
-
 </script>
-<style scoped>
-</style>
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-import LawList from './Laws/LawList.vue';
-const laws: string[] = ["Make It Obvious","Make It Attractive", "Make It Easy", "Make It Satisfying"]
-
+import LawList from './Laws/LawList.vue'
+const laws: string[] = [
+  'Make It Obvious',
+  'Make It Attractive',
+  'Make It Easy',
+  'Make It Satisfying'
+]
 </script>
 
 <template>
   <main>
-    <LawList :laws='laws'></LawList>
+    <LawList :laws="laws"></LawList>
   </main>
 </template>
